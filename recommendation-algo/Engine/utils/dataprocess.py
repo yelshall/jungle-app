56267a4fa686ff49dc21de6f160e47951a1a15a6
Yaodong Qiu <yaodongpurdue@gmail.com>
2021-10-24 21:03:57 -0400

    new util function to transfer data into sparse tensor

--- recommendation-algo/Engine/utils/dataprocess.py
+++ recommendation-algo/Engine/utils/dataprocess.py
@@ -4,9 +4,10 @@
 from os import uname
 import numpy as np
 import pandas as pd
+from pandas.core.indexes.base import Index
 import pymongo
 from pymongo import MongoClient
-import util
+#import util
 DATABASE_ACCESS = "mongodb+srv://yelshall:yyForever-53611@auth-test.p4buu.mongodb.net/db?retryWrites=true&w=majority"
 
 ## connect to the mongo database and return target table as mongo collection
@@ -20,12 +21,28 @@ def load_data(db,table):
 ## input database name
 ## return user tf
 def get_user():
-    return load_data("db","students")
+    return load_data("MIDB","users")
 
 ## input 
 ## return 
 def get_events():
-    return load_data("db","events")
+    return load_data("MIDB","events")
+
+## reconstruct df to 3 col: event_id, user_id, rating
+## if interestedEvents id with user_id fit with 1
+## if notInterestedEvents id with user_id fit with -1
+## otherwise fit with 0
+
+def reconstruct(users):
+    rating = users[["_id","interestedEvents"]]
+    users_ratings = pd.DataFrame(columns = get_events()["_id"], index = pd.Index(rating["_id"]))
+    print(users_ratings)
+    # each row correspond to a user rating
+    #ies = list(rating["interestedEvents"])
+    print(users_ratings.iloc[0:1,])
+    return 1
+
+print(reconstruct(get_user()))
 
 ## get full rating matrix of user and events
 ## if interested then mark as 1, if not mark as -1, if not reviewed mark as 0
@@ -34,9 +51,7 @@ def get_events():
 def get_rating_matrix(users):
     users_ratings = users[["_id","interestedEvents"]]
     return users_ratings
-    
 
-print(get_rating_matrix(get_user()))
 ## split data into train and test
 def split_data():
     return 0
