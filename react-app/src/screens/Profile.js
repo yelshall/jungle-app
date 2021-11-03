aef4e897875a9b4a7f45f105b27cf9f800e14c2b
albert3799 <albertibekwe@outlook.com>
2021-11-02 21:46:43 -0400

    Profile started list sucks

--- react-app/src/screens/Profile.js
+++ react-app/src/screens/Profile.js
@@ -1,6 +1,7 @@
-import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
+import { Text, View, TouchableOpacity, StyleSheet, Image } from "react-native";
 import { AuthContext } from "../utils/context";
 import React from "react";
+import { ListItem, Avatar } from "react-native-elements";
 
 import HostData from "../../assets/events-data/HostData";
 import eventsData from "../../assets/events-data/eventsData";
@@ -29,6 +30,19 @@ export default function Profile({ navigation, route }) {
     });
   };
 
+  const list = [
+    {
+      name: 'Amy Farha',
+      avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+      subtitle: 'Vice President'
+    },
+    {
+      name: 'Chris Jackson',
+      avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+      subtitle: 'Vice Chairman'
+    },
+  
+  ]
   return (
     <View
       style={{
@@ -38,21 +52,32 @@ export default function Profile({ navigation, route }) {
         justifyContent: "center",
       }}
     >
-      <TouchableOpacity style={styles.signOutBtn} onPress={onPref}>
-        <Text style={styles.signOutBtnText}>Preferences</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.signOutBtn} onPress={onFollowing}>
-        <Text style={styles.signOutBtnText}>Following</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.signOutBtn} onPress={onSignout}>
-        <Text style={styles.signOutBtnText}>Sign Out</Text>
-      </TouchableOpacity>
-
       {/*<Text style={{ margin: 90, fontSize: 28 }}>Profile</Text>
 			<LottieView
 				source={require("../../assets/Lottie/Profile.json")}
 				autoPlay
 			/>*/}
+
+      <Image
+        style={{ width: 150, height: 150, borderRadius: 75 }}
+        source={{
+          uri: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTUzMDg3Nzk3MDA2MTgxMzgz/madeline-mccann_credit-to-mccann-family-500.jpg",
+        }}
+      ></Image>
+      <Text>Maddy Mcanan</Text>
+      <Text>Following: 120</Text>
+
+
+        {list.map((l, i) => (
+          <ListItem key={i} bottomDivider>
+            <Avatar source={{ uri: l.avatar_url }} />
+            <ListItem.Content>
+              <ListItem.Title>{l.name}</ListItem.Title>
+              <ListItem.Subtitle>{l.subtitle}</ListItem.Subtitle>
+            </ListItem.Content>
+          </ListItem>
+        ))}
+
     </View>
   );
 }
@@ -78,3 +103,15 @@ const styles = StyleSheet.create({
     color: "#2f402d",
   },
 });
+
+//      <TouchableOpacity style={styles.signOutBtn} onPress={onPref}>
+{
+  /* <Text style={styles.signOutBtnText}>Preferences</Text>
+</TouchableOpacity>
+<TouchableOpacity style={styles.signOutBtn} onPress={onFollowing}>
+  <Text style={styles.signOutBtnText}>Following</Text>
+</TouchableOpacity>
+<TouchableOpacity style={styles.signOutBtn} onPress={onSignout}>
+  <Text style={styles.signOutBtnText}>Sign Out</Text>
+</TouchableOpacity> */
+}
