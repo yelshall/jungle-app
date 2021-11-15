b39c430ad8c3e7bb3ae6d14afdbaae5961294422
Yaodong Qiu <yaodongpurdue@gmail.com>
2021-11-14 22:57:58 -0500

    refactor this folder

--- recommendation-algo/Engine/engine.py
+++
@@ -1,9 +0,0 @@
-## import independency
-## Build and compile model
-## fit predict and evaluate
-## saving and reloading
-import tensorflow as tf
-
-#return a list of a events for each user
-def predict(user_embeddings,movie_embeddings):
-    return
